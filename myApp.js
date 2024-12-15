e9d06249449dce0edd56210a16e340e6c19190c8
catapplesofa <166747414+catapplesofa@users.noreply.github.com>
2024-12-15 15:31:25 -0500

    enforece hsts

--- myApp.js
+++ myApp.js
@@ -8,6 +8,14 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(
+  helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: true, // Override Gitpod's default HSTS settings
+  })
+);
+
 
 
 
